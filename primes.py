153f34508aaed5cd01766820132eb002ddafe776
Allister Gough <allistergough02@gmail.com>
2022-10-27 14:52:12 +0100

    Finish solution

--- primes.py
+++ primes.py
@@ -2,5 +2,12 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes <= 0:
+        raise ValueError(f"{number_of_primes} should have been a positive number greater than 0")
     list = []
+    with open('primes.txt', 'r') as p:
+        for i in range(number_of_primes):
+            prime = int(p.readline())
+            list.append(prime)
+
     return list
